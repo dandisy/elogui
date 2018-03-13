b92e2762e68633cc0491e2e8e0a8838180c3c323
dandi <dandi.webdev@gmail.com>
2018-03-13 10:22:56 +0700

    fixing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Elogui
-Laravel eloquent GUI package for webcore platform
+Laravel eloquent GUI package for webcore platform (a copy of https://github.com/dandisy/webcore-presentation)
--- src/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::get('/getTableColumn', 'Webcore\Elogui\Controllers\ColumnController@index');
-Route::resource('dataSources', 'Webcore\Elogui\Controllers\DataSourceController');
-Route::resource('dataQueries', 'Webcore\Elogui\Controllers\DataQueryController');
-Route::resource('columnAliases', 'Webcore\Elogui\Controllers\ColumnAliasController');
